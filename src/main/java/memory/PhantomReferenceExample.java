51e2ce107748ceeae4f46a0fc91915306eb179fc
Atul Singh <cocatul11@gmail.com>
2024-07-16 11:58:03 +0530

    Add PhantomReferenceExample class to demonstrate the use of PhantomReference in Java

--- src/main/java/memory/PhantomReferenceExample.java
+++ src/main/java/memory/PhantomReferenceExample.java
@@ -8,15 +8,23 @@ import java.lang.ref.ReferenceQueue;
 * */
 public class PhantomReferenceExample {
 	public static void main ( String[] args ) {
-		Employee emp = new Employee( "John", 23);
+		Employee emp = new Employee( "John", 23); // Strong Reference
+		System.out.println( "emp before phantom reference: " + emp ); // printing the object before phantom reference
 
-		ReferenceQueue<Employee> referenceQueue = new ReferenceQueue<>();
+		ReferenceQueue<Employee> referenceQueue = new ReferenceQueue<>(); // ReferenceQueue to store the reference of the object
 
-		PhantomReference<Employee> phantomReference = new PhantomReference<>(emp, referenceQueue);
-		System.out.println( "phantomReference.get() = " + phantomReference.get() );
+		PhantomReference<Employee> phantomReference = new PhantomReference<>(emp, referenceQueue); // Phantom Reference of the object
 
-//		emp = null;
-
-		System.out.println( "phantomReference.get() = " + phantomReference.get() );
+		emp = null; // Making the object eligible for garbage collection
+		emp = phantomReference.get(); // Trying to get the object from the phantom reference
+		System.out.println( "emp after phantom reference: " + emp ); // printing the object after phantom reference
 	}
 }
+/*
+* Phantom Reference is a type of reference in Java that is eagerly garbage collected. It is garbage collected as soon as the object is no longer reachable.
+* In this example, we create a new Employee object and create a PhantomReference to it. We then make the Employee object eligible for garbage collection by setting it to null.
+* We then try to get the object from the PhantomReference. If the object is still available, we print the object. If the object has been garbage collected, we print a message.
+* PhantomReference is useful when you want to perform some cleanup operation when an object is garbage collected. It is useful for resource management.
+ * The Employee object is garbage collected as soon as it is no longer reachable. The object is not available from the PhantomReference, so we print a message.
+* 
+* */
